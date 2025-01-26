6cd1c27d65c20b4ae563dd5b91361fdfc2a643b8
chani0548558941@gmail.com <chani0548558941@gmail.com>
2025-01-26 23:34:06 +0200

    first commit

---
+++ Serializer/Serializer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task<string> Load(string url)
+    {
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync(url);
+
+        // בדוק אם הבקשה הצליחה
+        if (response.IsSuccessStatusCode)
+        {
+            var html = await response.Content.ReadAsStringAsync();
+            return html;
+        }
+        else
+        {
+            return $"Error: {response.StatusCode}";
+        }
+    }
+
+    static async Task Main(string[] args)
+    {
+        string url = "https://www.10dakot.co.il/recipe"; // הכנס את ה-URL שלך כאן
+        string content = await Load(url);
+        Console.WriteLine(content); // הדפס את התוכן
+    }
+}
